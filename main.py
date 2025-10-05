c283e172ffc50c020b6f3cfbc899e771bc96fffb
Anton <Antonio.g.barone@outlook.com>
2025-10-04 22:27:06 -0400

    modified:   main.py

--- main.py
+++ main.py
@@ -5,6 +5,11 @@
 # Get input and convert to correct data type for testScore and classRank
 
 # Test using admission requirements and print Accept or Reject
+
+
+testScore = int(input("Enter test score: "))
+classRank = int(input("Enter class rank: "))
+
 if testScore >= 90:
   if classRank >= 25:
     print("Accept")
